b08398457fd2911488f5152378940bf719a0fdb5
Harry Hopkinson <harryjhopkinson@gmail.com>
2026-02-10 18:17:15 +0000

    Remove unused variables

--- src/recoil/Recoil.h
+++ src/recoil/Recoil.h
@@ -4,17 +4,9 @@
 
 #include <vector>
 
-extern std::vector<const char*> AttackerNames;
-extern std::vector<const char*> DefenderNames;
-
-extern std::vector<const char*> AttackerWeapons;
-extern std::vector<const char*> DefenderWeapons;
-
 extern WeaponRecoil CurrentRecoil;
 
 extern int ControllerMultiplier;
-
-extern int PresetIndex;
 
 std::pair<float, float> CalculateRecoil(
     float baseX, float baseY, float lookX, float lookY);
